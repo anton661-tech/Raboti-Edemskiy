a2e3d95c42936e196591a5f21709899115c57330
anton661-tech <antonzlheu@gmail.com>
2025-09-16 09:44:10 +0800

    Create S01.cpp

---
+++ S01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b,c,d;
+    cout << "input a = ";
+    cin >> a;
+    cout << "input b = ";
+    cin >> b;
+    c=a+b;
+    cout << "Summ " << c << endl;
+    cout << "Raznost' " << a-b << endl;
+    cout << "Proiz " << a*b << endl;
+    cout << "Proiz " << a/b << endl;
+
+
+    return 0;
+}
