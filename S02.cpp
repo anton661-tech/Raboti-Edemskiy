81347c1e11d21e15d84a0f7da01d8ca9a6635e43
anton661-tech <antonzlheu@gmail.com>
2025-09-16 09:52:01 +0800

    Create S02.cpp

---
+++ S02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cout << "input a = ";
+    cin >> a;
+    cout << "input b = ";
+    cin >> b;
+    c=a/b;
+    d=a%b;
+    cout << "delenie " << c<< endl;
+    cout << "ostatok " << d << endl;
+
+
+    return 0;
+}
